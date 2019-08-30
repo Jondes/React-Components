909accaa1337e3392310958299a6a42c27eeebc1
johnson guedes <jondes560@hotmail.com>
2019-08-30 12:47:17 +0100

    initial commit
    React components

---
+++ React-Data-Visual-master/website/webpack.config.js
@@ -0,0 +1,14 @@
+const {resolve} = require('path');
+
+module.exports = {
+  resolve: {
+    modules: [
+      resolve(__dirname, '../src'),
+      resolve(__dirname, '../node_modules')
+    ],
+
+    alias: {
+      react: resolve(__dirname, './node_modules/react')
+    }
+  }
+};
